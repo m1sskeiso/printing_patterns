aa0099066e80ec57ab7d2c25d419f511e1aea32c
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 21:06:18 +0700

    first

--- printing_patterns.py
+++ printing_patterns.py
@@ -1,4 +1,6 @@
 # Function to print the pattern
+def print_pattern(rows):
+    for i in range(1, rows + 1):
 # Print each number i, repeated i times
 # Specify the number of rows
 # Call the function to print the pattern
