6c143a0c074e2ac136cb5f1cdb753c5c7c8c657f
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-01-22 21:08:46 +0700

    final

--- printing_patterns.py
+++ printing_patterns.py
@@ -2,6 +2,10 @@
 def print_pattern(rows):
     for i in range(1, rows + 1):
 # Print each number i, repeated i times
+         print(" ".join(str(i) * i))
 # Specify the number of rows
+num_rows = 9
+
 # Call the function to print the pattern
+print_pattern(num_rows)
 
